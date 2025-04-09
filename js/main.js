ed9a8b1e2849d8597fd1902531e29fff0008da11
Basheer Altawil <basheeraltaweel@gmail.com>
2025-04-09 06:27:53 +0200

    fewag

--- js/main.js
+++ js/main.js
@@ -1,53 +1,40 @@
-// main.js
-let isBlogVisible = {
-  blog1: false,
-  blog2: false
-};
+let isBlogVisible = false;
 
-function toggleBlog(event, blog) {
-  const blogContent = document.getElementById('full-' + blog);
+function toggleBlog(event) {
+  const blogContent = document.getElementById('full-blog');
   const button = event.target;  // Get the clicked button
 
   // Check if the blog content is currently visible
-  if (isBlogVisible[blog]) {
+  if (isBlogVisible) {
     // Hide the content and change the button text
     blogContent.style.display = 'none';
     button.innerText = 'Read More...';
   } else {
     // If the content is not visible, load and show the content, change the button text
-    loadFullBlog(blog);
+    loadFullBlog();
     blogContent.style.display = 'block';
     button.innerText = 'Read Less...';
   }
 
   // Toggle visibility state
-  isBlogVisible[blog] = !isBlogVisible[blog];
+  isBlogVisible = !isBlogVisible;
 }
 
-function loadFullBlog(blog) {
-  let blogFile = '';
-  if (blog === 'blog1') {
-    blogFile = 'blogs/blog1.xml';
-  } else if (blog === 'blog2') {
-    blogFile = 'blogs/blog2.xml';
-  }
-
-  if (blogFile !== '') {
-    // Only load the blog content if it's not already loaded
-    const blogContent = document.getElementById('full-' + blog);
-    if (blogContent.innerHTML.trim() === '') {
-      fetch(blogFile)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.text();
-        })
-        .then(data => {
-          document.getElementById('full-' + blog).innerHTML = data;
-        })
-        .catch(error => console.error('Failed to load full blog:', error));
-    }
+function loadFullBlog() {
+  // Only load the content if it's not already loaded
+  const blogContent = document.getElementById('full-blog');
+  if (blogContent.innerHTML.trim() === '') {
+    fetch('blogs/blog1.html')  // Update the path as needed
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(data => {
+        blogContent.innerHTML = data;  // Insert blog content into the div
+      })
+      .catch(error => console.error('Failed to load full blog:', error));
   }
 }
 
