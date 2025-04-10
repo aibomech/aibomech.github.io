2c1da0c3e48a11e73523fe8e1849165574a1593e
Basheer Altawil <basheeraltaweel@gmail.com>
2025-04-10 21:33:10 +0200

    b3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let isBlogVisible = false;
 let isBlog2Visible = false;
-
+let isBlog3Visible = false;
 // Blog 1 Toggle
 function toggleBlog(event) {
   const blogContent = document.getElementById('full-blog');
@@ -68,6 +68,43 @@ function loadFullBlog2() {
       .catch(err => {
         blogContent.innerHTML = '<p>Failed to load blog content.</p>';
         console.error('Blog 2 error:', err);
+      });
+  }
+}
+
+
+// Blog 3 Toggle
+function toggleBlog3(event) {
+  const blogContent = document.getElementById('full-blog3');
+  const button = event.target;
+
+  if (isBlog2Visible) {
+    blogContent.style.display = 'none';
+    button.innerText = 'Read More...';
+  } else {
+    blogContent.style.display = 'block';
+    button.innerText = 'Read Less...';
+    loadFullBlog3();
+  }
+
+  isBlog2Visible = !isBlog2Visible;
+}
+
+function loadFullBlog3() {
+  const blogContent = document.getElementById('full-blog3');
+  if (blogContent.innerHTML.trim() === '') {
+    blogContent.innerHTML = '<p>Loading...</p>';
+    fetch('blogs/blog3.html')
+      .then(res => {
+        if (!res.ok) throw new Error('Network error');
+        return res.text();
+      })
+      .then(data => {
+        blogContent.innerHTML = data;
+      })
+      .catch(err => {
+        blogContent.innerHTML = '<p>Failed to load blog content.</p>';
+        console.error('Blog 3 error:', err);
       });
   }
 }
