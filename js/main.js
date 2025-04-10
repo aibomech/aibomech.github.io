b5c1b94690fcb791399e04a662ad3b02d227caf0
Basheer Altawil <basheeraltaweel@gmail.com>
2025-04-10 22:33:57 +0200

    GH

--- js/main.js
+++ js/main.js
@@ -1,199 +1,192 @@
-// Generalized Toggle Function
-function toggleBlog(event, blogId, buttonTextId, blogFile) {
-  const blogContent = document.getElementById(blogId);
+let isBlogVisible = false;
+let isBlog2Visible = false;
+let isBlog3Visible = false;
+let isBlog4Visible=false;
+let ismicrocotrollerVisible=false;
+// Blog 1 Toggle
+function toggleBlog(event) {
+  const blogContent = document.getElementById('full-blog');
   const button = event.target;
-  const isVisible = blogContent.style.display === 'block';
 
-  blogContent.style.display = isVisible ? 'none' : 'block';
-  button.innerText = isVisible ? 'Read More...' : 'Read Less...';
+  if (isBlogVisible) {
+    blogContent.style.display = 'none';
+    button.innerText = 'Read More...';
+  } else {
+    blogContent.style.display = 'block';
+    button.innerText = 'Read Less...';
+    loadFullBlog();
+  }
 
-  if (!isVisible && blogContent.innerHTML.trim() === '') {
-    blogContent.innerHTML = '<p>Loading...</p>';
-    fetch(blogFile)
-      .then(res => res.ok ? res.text() : Promise.reject('Network error'))
-      .then(data => blogContent.innerHTML = data)
+  isBlogVisible = !isBlogVisible;
+}
+
+function loadFullBlog() {
+  const blogContent = document.getElementById('full-blog');
+  if (blogContent.innerHTML.trim() === '') {
+    fetch('blogs/blog1.html')
+      .then(res => {
+        if (!res.ok) throw new Error('Network response was not ok');
+        return res.text();
+      })
+      .then(data => {
+        blogContent.innerHTML = data;
+      })
       .catch(err => {
         blogContent.innerHTML = '<p>Failed to load blog content.</p>';
-        console.error(err);
+        console.error('Blog 1 error:', err);
       });
   }
 }
 
-// Event Listeners for Blog Toggles
-document.getElementById('toggle-blog-1').addEventListener('click', function(event) {
-  toggleBlog(event, 'full-blog', 'toggle-blog-1', 'blogs/blog1.html');
-});
+// Blog 2 Toggle
+function toggleBlog2(event) {
+  const blogContent = document.getElementById('full-blog2');
+  const button = event.target;
 
-document.getElementById('toggle-blog-2').addEventListener('click', function(event) {
-  toggleBlog(event, 'full-blog2', 'toggle-blog-2', 'blogs/blog2.html');
-});
+  if (isBlog2Visible) {
+    blogContent.style.display = 'none';
+    button.innerText = 'Read More...';
+  } else {
+    blogContent.style.display = 'block';
+    button.innerText = 'Read Less...';
+    loadFullBlog2();
+  }
 
-document.getElementById('toggle-blog-3').addEventListener('click', function(event) {
-  toggleBlog(event, 'full-blog3', 'toggle-blog-3', 'blogs/blog3.html');
-});
+  isBlog2Visible = !isBlog2Visible;
+}
 
-document.getElementById('toggle-blog-4').addEventListener('click', function(event) {
-  toggleBlog(event, 'full-blog4', 'toggle-blog-4', 'blogs/blog4.html');
-});
+function loadFullBlog2() {
+  const blogContent = document.getElementById('full-blog2');
+  if (blogContent.innerHTML.trim() === '') {
+    blogContent.innerHTML = '<p>Loading...</p>';
+    fetch('blogs/blog2.html')
+      .then(res => {
+        if (!res.ok) throw new Error('Network error');
+        return res.text();
+      })
+      .then(data => {
+        blogContent.innerHTML = data;
+      })
+      .catch(err => {
+        blogContent.innerHTML = '<p>Failed to load blog content.</p>';
+        console.error('Blog 2 error:', err);
+      });
+  }
+}
+
+
+// Blog 3 Toggle
+function toggleBlog3(event) {
+  const blogContent = document.getElementById('full-blog3');
+  const button = event.target;
+
+  if (isBlog3Visible) {
+    blogContent.style.display = 'none';
+    button.innerText = 'Read More...';
+  } else {
+    blogContent.style.display = 'block';
+    button.innerText = 'Read Less...';
+    loadFullBlog3();
+  }
+
+  isBlog3Visible = !isBlog3Visible;
+}
+
+function loadFullBlog3() {
+  const blogContent = document.getElementById('full-blog3');
+  if (blogContent.innerHTML.trim() === '') {
+    blogContent.innerHTML = '<p>Loading...</p>';
+    fetch('blogs/blog3.html')
+      .then(res => {
+        if (!res.ok) throw new Error('Network error');
+        return res.text();
+      })
+      .then(data => {
+        blogContent.innerHTML = data;
+      })
+      .catch(err => {
+        blogContent.innerHTML = '<p>Failed to load blog content.</p>';
+        console.error('Blog 3 error:', err);
+      });
+  }
+}
+
+
+// Blog 4 Toggle
+function toggleBlog4(event) {
+  const blogContent = document.getElementById('full-blog4');
+  const button = event.target;
+
+  if (isBlog4Visible) {
+    blogContent.style.display = 'none';
+    button.innerText = 'Read More...';
+  } else {
+    blogContent.style.display = 'block';
+    button.innerText = 'Read Less...';
+    loadFullBlog4();
+  }
+
+  isBlog4Visible = !isBlog4Visible;
+}
+
+function loadFullBlog4() {
+  const blogContent = document.getElementById('full-blog4');
+  if (blogContent.innerHTML.trim() === '') {
+    blogContent.innerHTML = '<p>Loading...</p>';
+    fetch('blogs/blog4.html')
+      .then(res => {
+        if (!res.ok) throw new Error('Network error');
+        return res.text();
+      })
+      .then(data => {
+        blogContent.innerHTML = data;
+      })
+      .catch(err => {
+        blogContent.innerHTML = '<p>Failed to load blog content.</p>';
+        console.error('Blog 4 error:', err);
+      });
+  }
+}
+
+// loadFulltogglemicrocotroller
+// micorcontrooler Toggle
+function togglemicrocotroller(event) {
+  const blogContent = document.getElementById('full-microcotroller');
+  const button = event.target;
+
+  if (ismicrocotrollerVisible) {
+    blogContent.style.display = 'none';
+    button.innerText = 'Read More...';
+  } else {
+    blogContent.style.display = 'block';
+    button.innerText = 'Read Less...';
+    loadFulltogglemicrocotroller();
+  }
+
+  loadFulltogglemicrocotroller = !loadFulltogglemicrocotroller;
+}
+
+function loadFulltogglemicrocotroller() {
+  const blogContent = document.getElementById('full-micorcontrooler');
+  if (blogContent.innerHTML.trim() === '') {
+    blogContent.innerHTML = '<p>Loading...</p>';
+    fetch('blogs/microcontroller.html')
+      .then(res => {
+        if (!res.ok) throw new Error('Network error');
+        return res.text();
+      })
+      .then(data => {
+        blogContent.innerHTML = data;
+      })
+      .catch(err => {
+        blogContent.innerHTML = '<p>Failed to load blog content.</p>';
+        console.error('microcontroller error:', err);
+      });
+  }
+}
+
 
 // Dark Mode Toggle
-document.getElementById('dark-mode-toggle').addEventListener('click', () => {
+function toggleDarkMode() {
   document.body.classList.toggle('dark-mode');
-});
-
-
-
-
-// let isBlogVisible = false;
-// let isBlog2Visible = false;
-// let isBlog3Visible = false;
-// let isBlog4Visible=false;
-// // Blog 1 Toggle
-// function toggleBlog(event) {
-//   const blogContent = document.getElementById('full-blog');
-//   const button = event.target;
-
-//   if (isBlogVisible) {
-//     blogContent.style.display = 'none';
-//     button.innerText = 'Read More...';
-//   } else {
-//     blogContent.style.display = 'block';
-//     button.innerText = 'Read Less...';
-//     loadFullBlog();
-//   }
-
-//   isBlogVisible = !isBlogVisible;
-// }
-
-// function loadFullBlog() {
-//   const blogContent = document.getElementById('full-blog');
-//   if (blogContent.innerHTML.trim() === '') {
-//     fetch('blogs/blog1.html')
-//       .then(res => {
-//         if (!res.ok) throw new Error('Network response was not ok');
-//         return res.text();
-//       })
-//       .then(data => {
-//         blogContent.innerHTML = data;
-//       })
-//       .catch(err => {
-//         blogContent.innerHTML = '<p>Failed to load blog content.</p>';
-//         console.error('Blog 1 error:', err);
-//       });
-//   }
-// }
-
-// // Blog 2 Toggle
-// function toggleBlog2(event) {
-//   const blogContent = document.getElementById('full-blog2');
-//   const button = event.target;
-
-//   if (isBlog2Visible) {
-//     blogContent.style.display = 'none';
-//     button.innerText = 'Read More...';
-//   } else {
-//     blogContent.style.display = 'block';
-//     button.innerText = 'Read Less...';
-//     loadFullBlog2();
-//   }
-
-//   isBlog2Visible = !isBlog2Visible;
-// }
-
-// function loadFullBlog2() {
-//   const blogContent = document.getElementById('full-blog2');
-//   if (blogContent.innerHTML.trim() === '') {
-//     blogContent.innerHTML = '<p>Loading...</p>';
-//     fetch('blogs/blog2.html')
-//       .then(res => {
-//         if (!res.ok) throw new Error('Network error');
-//         return res.text();
-//       })
-//       .then(data => {
-//         blogContent.innerHTML = data;
-//       })
-//       .catch(err => {
-//         blogContent.innerHTML = '<p>Failed to load blog content.</p>';
-//         console.error('Blog 2 error:', err);
-//       });
-//   }
-// }
-
-
-// // Blog 3 Toggle
-// function toggleBlog3(event) {
-//   const blogContent = document.getElementById('full-blog3');
-//   const button = event.target;
-
-//   if (isBlog3Visible) {
-//     blogContent.style.display = 'none';
-//     button.innerText = 'Read More...';
-//   } else {
-//     blogContent.style.display = 'block';
-//     button.innerText = 'Read Less...';
-//     loadFullBlog3();
-//   }
-
-//   isBlog3Visible = !isBlog3Visible;
-// }
-
-// function loadFullBlog3() {
-//   const blogContent = document.getElementById('full-blog3');
-//   if (blogContent.innerHTML.trim() === '') {
-//     blogContent.innerHTML = '<p>Loading...</p>';
-//     fetch('blogs/blog3.html')
-//       .then(res => {
-//         if (!res.ok) throw new Error('Network error');
-//         return res.text();
-//       })
-//       .then(data => {
-//         blogContent.innerHTML = data;
-//       })
-//       .catch(err => {
-//         blogContent.innerHTML = '<p>Failed to load blog content.</p>';
-//         console.error('Blog 3 error:', err);
-//       });
-//   }
-// }
-
-
-// // Blog 4 Toggle
-// function toggleBlog4(event) {
-//   const blogContent = document.getElementById('full-blog4');
-//   const button = event.target;
-
-//   if (isBlog4Visible) {
-//     blogContent.style.display = 'none';
-//     button.innerText = 'Read More...';
-//   } else {
-//     blogContent.style.display = 'block';
-//     button.innerText = 'Read Less...';
-//     loadFullBlog4();
-//   }
-
-//   isBlog4Visible = !isBlog4Visible;
-// }
-
-// function loadFullBlog4() {
-//   const blogContent = document.getElementById('full-blog4');
-//   if (blogContent.innerHTML.trim() === '') {
-//     blogContent.innerHTML = '<p>Loading...</p>';
-//     fetch('blogs/blog4.html')
-//       .then(res => {
-//         if (!res.ok) throw new Error('Network error');
-//         return res.text();
-//       })
-//       .then(data => {
-//         blogContent.innerHTML = data;
-//       })
-//       .catch(err => {
-//         blogContent.innerHTML = '<p>Failed to load blog content.</p>';
-//         console.error('Blog 4 error:', err);
-//       });
-//   }
-// }
-
-// // Dark Mode Toggle
-// function toggleDarkMode() {
-//   document.body.classList.toggle('dark-mode');
-// }
+}
